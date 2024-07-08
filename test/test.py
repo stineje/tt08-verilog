9aab2726cccae89d5289d04a91df5b0b50628103
James E. Stine <james.stine@okstate.edu>
2024-07-08 16:24:49 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -20,7 +20,7 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 2)
+    await ClockCycles(dut.clk, 3)
     dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
