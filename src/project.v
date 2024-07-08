75deb778ed84e916e825c83861a50d06a5cbba71
James E. Stine <james.stine@okstate.edu>
2024-07-08 15:31:00 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -23,7 +23,7 @@ module tt_um_example (
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
-  assign uio_out = ui_in;
+  assign uo_out = ui_in;
 
 
 endmodule
