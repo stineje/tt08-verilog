b0f6e7e703dcf29db6a9ff5a9f717a711bb59d65
James E. Stine <james.stine@okstate.edu>
2024-07-08 16:30:43 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -30,7 +30,7 @@ async def test_project(dut):
     dut.uio_in.value = 30 
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, (1 << 10))
+    await ClockCycles(dut.clk, (1 << 20))
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
